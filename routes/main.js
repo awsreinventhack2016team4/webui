d95c882418d4dc165f10f180e2b9edef1958e025
Tronson, Jonathan L <JLTronson@DIRECTV.com>
2016-11-28 16:39:31 -0800

    Filters finished

--- routes/main.js
+++ routes/main.js
@@ -13,6 +13,27 @@ exports.home = function(req, res) {
   var hostname = os.hostname();
   res.header('Server', hostname);
 
-  res.render('index', { title: 'Hackathon'});
+  rest.get('https://s3-us-west-2.amazonaws.com/wcs-giraffe/data/sample-api-data/Weapons+and+Gear+Seized.json',
+  {data: 'Content-Type: application/json'}).once('complete', function(data1, response) {
+
+    rest.get('https://s3-us-west-2.amazonaws.com/wcs-giraffe/data/sample-api-data/Wildlife.json',
+    {data: 'Content-Type: application/json'}).once('complete', function(data2, response) {
+
+      rest.get('https://s3-us-west-2.amazonaws.com/wcs-giraffe/data/sample-api-data/People+Direct+Observation.json',
+      {data: 'Content-Type: application/json'}).once('complete', function(data3, response) {
+
+        //logger.info(util.inspect(patrolData));
+        res.render('index', { title: 'Hackathon',
+         weapons: data1,
+         wildlife: data2,
+         people: data3
+        });
+
+      });
+
+
+    });
+
+  });
 
 }
